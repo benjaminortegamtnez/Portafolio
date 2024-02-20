092bd1b053db277acd6cc4f6c203d3ce68a82262
Benjamín Ortega´ <benjaminortega.mtnez@gmail.com>
2024-02-20 09:16:08 -0600

    Avance Portafolio-WebSite

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,97 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Creativo</li>
+        <li>Trabajo en equipo</li>
+        <li>Pensamiento crítico</li>
+        <li>Rápido Aprendizaje</li>
+        <li>Proactividad</li>
+        <li>Capacidad de Adaptación</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Academy of Code</li>
+        <li>Universidad Autónoma Metropolitana, Cuajimalpa</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/developer.JPG" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">Sobre mí</h2>
+          <p className="text-base lg:text-lg">
+            Soy un desarrollador Full Stack con Licenciatura en Diseño, dedicado a
+            la constante búsqueda de conocimiento en el ámbito de las tecnologías
+            de la información. Abordo problemas con enfoque creativo para ofrecer
+            soluciones a diversas necesidades mediante el diseño.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,58 @@
+"use client"
+import React from 'react';
+import Image from 'next/image';
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className='grid grid-cols-1 sm:grid-cols-12'>
+            <div className='col-span-7 place-self-center text-center sm:text-left'>
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 via-cyan-500 to-indigo-600">
+                        Hello, I'am{" "}
+                        </span>
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                "Benjamín",
+                                1000,
+                                "Diseñador",
+                                1000,
+                                "Desarrollador",
+                                1000,
+                                "Full Stack",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />  
+                </h1>
+                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt accusantium provident quisquam dolorem quae, cum sit et harum quasi eos atque pariatur quis magnam voluptatem. Numquam, tenetur. Nisi, dolores nostrum.
+                </p>
+                <div>
+                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-cyan-500 to-indigo-600 hover:bg-slate-200 text-white">Hire Me</button>
+                    <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-cyan-500 to-indigo-600 hover:bg-slate-800 text-white mt-3">
+                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Descargar CV</span>
+                    </button>
+                </div>
+            </div>
+            <div className="col-span-4 place-self-center mt-4 lg:mt-0">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[330px] lg:h-[330px] relative">
+                    <Image
+                        src="/images/benjamin_c2.png"
+                        alt='benjamin ortega mtz'
+                        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        width={300}
+                        height={300}
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
