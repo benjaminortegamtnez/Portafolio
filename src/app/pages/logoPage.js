6826410e10267c4abfbb8eebc14e1a8d7f2bf8a8
Benjamín Ortega´ <benjaminortega.mtnez@gmail.com>
2024-02-23 12:23:09 -0600

    Nueva metodologia implementada en la sección de diseño

--- src/app/pages/logoPage.js
+++ src/app/pages/logoPage.js
@@ -1,19 +1,19 @@
-import React from "react";
-import Navbar from "@/components/Navbar";
-import DisenoLogoCards from "../../components/DisenoLogoCards";
-import Footer from "@/components/Footer";
+// import React from "react";
+// import Navbar from "@/components/Navbar";
+// import DisenoLogoCards from "../../components/DisenoLogoCards";
+// import Footer from "@/components/Footer";
 
-const logoPage = () => {
-  return (
-    <main className="flex min-h-screen flex-col bg-[#121212]" id="logoPage">
-      <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <Navbar />
-            <DisenoLogoCards />
-        <Footer />
-        </div>
-    </main>
-  );
-};
+// const logoPage = () => {
+//   return (
+//     <main className="flex min-h-screen flex-col bg-[#121212]" id="logoPage">
+//       <Navbar />
+//       <div className="container mt-24 mx-auto px-12 py-4">
+//         <Navbar />
+//             <DisenoLogoCards />
+//         <Footer />
+//         </div>
+//     </main>
+//   );
+// };
 
-export default logoPage;
+// export default logoPage;
