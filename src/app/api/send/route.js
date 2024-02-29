3163fbf27ba108ba739df5626a98370ef3ca0cc9
Benjamín Ortega´ <benjaminortega.mtnez@gmail.com>
2024-02-29 12:34:36 -0600

    prueba 5 contacto

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,22 +1,23 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
+
 import { Resend } from 'resend';
 
-const resend = new Resend("re_MHJUoBwH_PmgZzJnXqPVNsX7h2pX4jyNS");
+const resend = new Resend(process.env.RESEND_API_KEY="re_MHJUoBwH_PmgZzJnXqPVNsX7h2pX4jyNS");
 
 export async function POST() {
   try {
     const data = await resend.emails.send({
-      from: 'Benjamín <benjaminortega.mtnez@gmail.com>',
-      to: ['benjamin.graphic@gmail.com'],
-      subject: 'Hello world',
+      from: 'Ben <onboarding@resend.dev>',
+      to: ['benjaminortega.mtnez@gmail.com'],
+      subject: 'Correo Portafolio',
       react: 
         <>
           <p>Email body</p>
         </>,
     });
 
-    return Response.json(data);
+    return new Response(JSON.stringify(data), { status: 200, headers: { 'Content-Type': 'application/json' } });
   } catch (error) {
-    return Response.json({ error });
+    console.error("Error sending email:", error);
+    return new Response(JSON.stringify({ error: "Failed to send email." }), { status: 500, headers: { 'Content-Type': 'application/json' } });
   }
 }
